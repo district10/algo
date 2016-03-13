3a0956f1b57295e5a959e6407d508681cb2743b1
Tang Zhixiong <gnat_tang@yeah.net>
2016-03-13 22:55:15 +0800

    stddef.h for size_t

--- src/SimpleStdCpp/SimpleStdCpp.h
+++ src/SimpleStdCpp/SimpleStdCpp.h
@@ -1,10 +1,12 @@
 #ifndef ALGO_SIMPLE_STD_CPP_H
 #define ALGO_SIMPLE_STD_CPP_H
 
+#include <cstddef>
+
 namespace algo {
 
 size_t strlen( const char *str );
-char *strcpy( char *to, const char *from  );
+char *strcpy( char *to, const char *from );
 int atoi( const char *str );
 int kmp( const char *text, const char *pattern );
 
