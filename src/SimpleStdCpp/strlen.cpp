a4839f7444686fe408caeff888c71c1d5c14d81e
TANG ZhiXiong (dvorak4tzx) <district10@users.noreply.github.com>
2016-04-14 22:07:20 +0800

    Update strlen.cpp

--- src/SimpleStdCpp/strlen.cpp
+++ src/SimpleStdCpp/strlen.cpp
@@ -6,3 +6,11 @@ size_t algo::strlen( const char *str )
     for( s=str; *s; ++s ) { }
     return s-str;
 }
+
+// alternative 1
+size_t also::strlen_alt1( const char *str )
+{
+    size_t i = 0;
+    while(*str++) { ++i; }
+    return i;
+}
